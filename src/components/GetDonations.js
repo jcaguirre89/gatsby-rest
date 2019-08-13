94b51af6f767deff6053ec343ea6766c0ad7cdea
Cristobal Aguirre <cristobal.aguirre89@gmail.com>
2019-08-12 23:20:19 -0700

    small fix

--- src/components/GetDonations.js
+++ src/components/GetDonations.js
@@ -4,15 +4,23 @@ import {useQuery} from '@apollo/react-hooks'
 
 const GET_DONATIONS = gql`
 query GET_DONATIONS {
-  donations(page: 1) {
-    nombres
-  }
-}
+donativos(page:1) {
+  id_donativo
+  nombres
+  apellidos
+  pais
+  cargo
+  ocasion
+  descripcion
+  fecha
+  nombre_institucion
+}}
 `
 
 export default function GetDonations() {
   const {loading, error, data} = useQuery(GET_DONATIONS)
 
+  console.log(error)
   console.log(data)
   return (
     <div>
