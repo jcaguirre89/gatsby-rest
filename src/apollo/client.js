d10fd8dfdb6bd136dd71d7ddc72c5bd97b11d48c
Cristobal Aguirre <cristobal.aguirre89@gmail.com>
2019-08-12 23:39:53 -0700

    small change

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -1,7 +1,11 @@
 import ApolloClient from 'apollo-boost';
 import fetch from 'isomorphic-fetch';
 
+const URL = process.env.NODE_ENV === 'development' ?
+  "http://172.25.201.32:9000/.netlify/functions/graphql" :
+  "/.netlify/functions/graphql"
+
 export const client = new ApolloClient({
-  uri: "/.netlify/functions/graphql",
+  uri: URL,
   fetch,
 });
